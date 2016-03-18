038823531376766312faa0ec03797ceef1fb72a3
zhengshizhao <zhao.zhengshi@gmail.com>
2016-03-17 20:58:02 -0400

    added initial settings to models fields

--- tests/server/models/user-test.js
+++ tests/server/models/user-test.js
@@ -1,4 +1,4 @@
-var dbURI = 'mongodb://localhost:27017/testingDB-evercode';
+var dbURI = 'mongodb://localhost:27017/testingDB';
 var clearDB = require('mocha-mongoose')(dbURI);
 
 var sinon = require('sinon');
