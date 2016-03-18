e7bef95522f20dcb426cc2b57c31e2d3913944cc
zhengshizhao <zhao.zhengshi@gmail.com>
2016-03-18 17:07:09 -0400

    set route for public notebooks and notes

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -5,7 +5,9 @@ module.exports = router;
 router.use('/members', require('./members'));
 router.use('/users', require('./users'));
 router.use('/:userId/notebooks', require('./notebooks'));
+router.use('/notebooks/public', require('./notebooks/public'));
 router.use('/:userId/notes', require('./notes'));
+router.use('/notes/public', require('./notes/public'));
 
 // Make sure this is after all of
 // the registered routes!
