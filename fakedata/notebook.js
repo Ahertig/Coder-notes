59ec6ec01d7b1df48cb017b1f6505fa558b431c0
zhengshizhao <zhao.zhengshi@gmail.com>
2016-03-19 12:09:02 -0400

    add seed data for user/notebooks/notes

---
+++ fakedata/notebook.js
@@ -0,0 +1,152 @@
+module.exports = [
+	{
+		"title": "iaculis odio."
+	},
+	{
+		"title": "luctus lobortis."
+	},
+	{
+		"title": "Nam ac"
+	},
+	{
+		"title": "magna. Cras"
+	},
+	{
+		"title": "erat nonummy"
+	},
+	{
+		"title": "Nullam lobortis"
+	},
+	{
+		"title": "dis parturient"
+	},
+	{
+		"title": "Nam porttitor"
+	},
+	{
+		"title": "Duis gravida."
+	},
+	{
+		"title": "nec, mollis"
+	},
+	{
+		"title": "non arcu."
+	},
+	{
+		"title": "Duis mi"
+	},
+	{
+		"title": "Duis at"
+	},
+	{
+		"title": "semper pretium"
+	},
+	{
+		"title": "ornare. In"
+	},
+	{
+		"title": "Donec nibh"
+	},
+	{
+		"title": "Nunc mauris"
+	},
+	{
+		"title": "Duis gravida."
+	},
+	{
+		"title": "mauris elit,"
+	},
+	{
+		"title": "Sed diam"
+	},
+	{
+		"title": "Pellentesque ut"
+	},
+	{
+		"title": "nec, cursus"
+	},
+	{
+		"title": "neque non"
+	},
+	{
+		"title": "penatibus et"
+	},
+	{
+		"title": "tempor lorem,"
+	},
+	{
+		"title": "Lorem ipsum"
+	},
+	{
+		"title": "In nec"
+	},
+	{
+		"title": "ligula eu"
+	},
+	{
+		"title": "scelerisque scelerisque"
+	},
+	{
+		"title": "tellus, imperdiet"
+	},
+	{
+		"title": "lobortis quis,"
+	},
+	{
+		"title": "Quisque imperdiet,"
+	},
+	{
+		"title": "Nulla tempor"
+	},
+	{
+		"title": "malesuada malesuada."
+	},
+	{
+		"title": "Donec non"
+	},
+	{
+		"title": "libero est,"
+	},
+	{
+		"title": "leo. Vivamus"
+	},
+	{
+		"title": "interdum ligula"
+	},
+	{
+		"title": "Vestibulum ut"
+	},
+	{
+		"title": "Vivamus molestie"
+	},
+	{
+		"title": "Nulla aliquet."
+	},
+	{
+		"title": "magnis dis"
+	},
+	{
+		"title": "nulla ante,"
+	},
+	{
+		"title": "Maecenas malesuada"
+	},
+	{
+		"title": "tincidunt, nunc"
+	},
+	{
+		"title": "Sed auctor"
+	},
+	{
+		"title": "et tristique"
+	},
+	{
+		"title": "adipiscing elit."
+	},
+	{
+		"title": "Pellentesque tincidunt"
+	},
+	{
+		"title": "a, auctor"
+	}
+]
