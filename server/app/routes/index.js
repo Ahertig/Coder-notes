a8efb370702dac8671f23461ad8e57af7694eebe
dorothym <dorothy.v.moore@gmail.com>
2016-03-19 15:18:18 -0400

    uninstall angular materials. switch back to bootstrap framework

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -4,7 +4,7 @@ module.exports = router;
 
 router.use('/members', require('./members'));
 router.use('/users', require('./users'));
-router.use('/public', require('./public'));
+// router.use('/public', require('./public'));
 router.use('/notes', require('./notes/public.notes.js'));
 
 
