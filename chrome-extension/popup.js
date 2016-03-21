5c5474bf918a352d02f330ae6d250c3536f7a6e2
Ahertig <alyssa.hertig@gmail.com>
2016-03-21 12:36:57 -0400

    successfully retrieves notebooks for hardcoded ids

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -1,5 +1,3 @@
-console.log(this);
-
 $(document).ready(function(){
 
   // Retrieve login information
@@ -18,18 +16,37 @@ $(document).ready(function(){
   });
 
   // Testing with public notebooks route: Retrieve notebooks so that user can choose which notebook to add a note to
-  $.ajax({
-    url: "http://localhost:1337/api/public/notebooks/all",
-    type: "GET",
-    dataType: 'jsonp',
-    success: function(data) {
-      console.log(data);
-      $.each(data, function(notebook) {
-        var notebook = "<option>" + notebook.title + "</option>"
-        $(notebook).appendTo("#notebook option");
-      })
+  // $.ajax({
+  //   url: "http://localhost:1337/api/users/56edbb745da4ce9ea00313a8/notebooks/own",
+  //   type: "GET",
+  //   dataType: 'jsonp',
+  //   success: function(data) {
+  //     console.log(data);
+  //     $.each(data, function(notebook) {
+  //       var notebook = "<option>" + notebook.title + "</option>"
+  //       $(notebook).appendTo("#notebook option");
+  //     })
+  //   }
+  // });
+
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", 'http://localhost:1337/api/users/56edbb745da4ce9ea00313a8/notebooks/own', true);
+  xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
+  xhr.onreadystatechange = function() {//Call a function when the state changes.
+    if(xhr.readyState == 4 && xhr.status == 200) {
+      console.log('text', xhr.responseText)
+      var notebooksJSON = JSON.parse(xhr.responseText);
+
+      for (var i = 0; i < notebooksJSON.length; i++) {
+        console.log(notebooksJSON[i].title)
+        var notebook = "<option>" + notebooksJSON[i].title + "</option>"
+        $(notebook).appendTo("#notebook");
+      }
     }
-  });
+  }
+
+  xhr.send();
 
 });
 
