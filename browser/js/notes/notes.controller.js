0ad6bf146fcbc04fa7b465923867ff8508fe4b40
zhengshizhao <zhao.zhengshi@gmail.com>
2016-03-21 15:50:42 -0400

    save note button works

--- browser/js/notes/notes.controller.js
+++ browser/js/notes/notes.controller.js
@@ -11,6 +11,7 @@ app.controller('NotesCtrl', function($scope, AuthService, myNotebooks, NotesFact
     // set the 'current note' to item 0 in the array by default. 
     // not sure if this logic is correct.
     $rootScope.currentNote = $scope.notes[0];
+    $rootScope.currentNotebook = $scope.notebooks[0];
 
 	$scope.isLoggedIn = function () {
 		return AuthService.isAuthenticated();
--- browser/js/notes/singlenote/singlenote.controller.js
+++ browser/js/notes/singlenote/singlenote.controller.js
@@ -1,23 +1,56 @@
-app.controller('SingleNoteCtrl', function($scope, $rootScope) {
+app.controller('SingleNoteCtrl', function($scope, $rootScope, NotesFactory) {
+	$scope.savenote = {};
+	
+	$scope.stroutput = "";
+
 	$scope.consolelogdom = function() {
 		var childArray = $('article').children();
 		// var childArray = $('article').children().outerHTML();
-		var stroutput = "";
+		
 		// console.log("Here is the text:",text)
 		for(var i = 0; i < childArray.length; i++) {
-			// console.log("child",i,"is",childArray[i].html());
-				// console.log("child",i,"is",childArray[i]);
-				console.log("outerhtml",childArray[i].outerHTML)
-				stroutput += childArray[i].outerHTML;
-			// console.log("childArray is", childArray)
-			// console.log("childArray[1] is", childArray[1])
-		}
-		console.log("final string output",stroutput)
+		// 	// console.log("child",i,"is",childArray[i].html());
+		// 		// console.log("child",i,"is",childArray[i]);
+		 	
+		 	
+		    $scope.stroutput += childArray[i].outerHTML;
+		    console.log("outerhtml",$scope.stroutput);
+		// 	// console.log("childArray is", childArray)
+		// 	console.log("childArray", i ,"is", childArray[i].innerHTML)
+		 }
+		// //console.log("final string output",childArray)
 		// console.log("entire childArray",childArray)
-	}
+		//console.log("user ID, Note ID and NotebookID",$scope.user._id,$scope.currentNote._id,$scope.notebook._id);
+		//console.log("UserID, currentNoteID,currentNotebookID ", $scope.user._id,$rootScope.currentNote._id,$rootScope.currentNotebook._id); 
 
-	$scope.save = function() {
-		
+		//console.log("user ID, Note ID and NotebookID",$scope.currentNote._id,$rootScope.notebook._id);
+		console.log("final string",$scope.stroutput)
 	}
+	
+
+	// $scope.save = function() {
+		
+	// }
+	// $scope.savenote.subject = $rootScope.currentNote.subject;
+	// $scope.savenote.body = $scope.stroutput;
+	// $scope.savenote.lastUpdate = new Date()
+
+
+
+	var userID = $scope.user._id;
+	var noteID = $rootScope.currentNote._id;
+	var notebookID = $rootScope.currentNotebook._id;
+
+	//console.log("user ID, Note ID and NotebookID",userID, noteID);
+    $scope.save = function(){ 
+		$scope.savenote = {
+			"subject": $rootScope.currentNote.subject,
+			"body": $scope.stroutput
+		}
+    	console.log("update note: stroutput:",$scope.stroutput, "savenote:",$scope.savenote )
+    	// console.log("NotebookID",$rootScope.currentNotebook._id);
+     	NotesFactory.saveNote(userID, notebookID,noteID, $scope.savenote);
+     }
+	
 
 })
