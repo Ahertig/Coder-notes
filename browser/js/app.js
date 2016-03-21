c6889a57ad63a1ee8995dfcfda9cc8778c9c2ff4
zhengshizhao <zhao.zhengshi@gmail.com>
2016-03-21 00:47:17 -0400

    test-highlight

--- browser/js/app.js
+++ browser/js/app.js
@@ -1,11 +1,15 @@
 'use strict';
-window.app = angular.module('FullstackGeneratedApp', ['fsaPreBuilt', 'ui.router', 'ui.bootstrap', 'ngAnimate']);
+window.app = angular.module('FullstackGeneratedApp', ['fsaPreBuilt', 'ui.router', 'ui.bootstrap', 'ngAnimate','hljs']);
 
-app.config(function ($urlRouterProvider, $locationProvider) {
+app.config(function ($urlRouterProvider, $locationProvider,hljsServiceProvider) {
     // This turns off hashbang urls (/#about) and changes it to something normal (/about)
     $locationProvider.html5Mode(true);
     // If we go to a URL that ui-router doesn't have registered, go to the "/" url.
     $urlRouterProvider.otherwise('/');
+    hljsServiceProvider.setOptions({
+    // replace tab with 4 spaces
+        tabReplace: '    '
+  });
 });
 
 // This app.run is for controlling access to specific states.
--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -1,6 +1,39 @@
 app.config(function ($stateProvider) {
     $stateProvider.state('home', {
         url: '/',
-        templateUrl: 'js/home/home.html'
+        templateUrl: 'js/home/home.html',
+        controller:'homeCrl'
     });
 });
+
+app.controller('homeCrl', function($scope,$window){
+	$scope.hljs = $window.hljs;
+	$scope.iscode = false;
+	$scope.iscodemode = function(){
+		$scope.iscode = !$scope.iscode;
+		console.log("is codemode");
+		
+	}
+	
+})
+
+app.directive('codeMode',function($window){
+	return {
+		restrict: 'E',
+		templateUrl:'js/home/test-highlight.html',
+		scope:{
+			iscode:'='
+		},
+		link: function(scope,ele,attr){
+			//scope.hljs.initHighlightingOnLoad();
+
+			
+			scope.text = ele[0].innerText;
+			console.log("element", ele);
+			//scope.hljs.highlight('js','scope.text');
+
+
+		}
+
+	}
+})
