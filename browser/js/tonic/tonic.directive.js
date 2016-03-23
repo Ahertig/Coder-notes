5d1f71c922a7d6c087d1e551c4f3ac5d78e53ef4
Ksenia Mikhailovskaya <ksenia0822@gmail.com>
2016-03-22 21:24:06 -0400

    testing eval and tonic

--- browser/js/tonic/tonic.directive.js
+++ browser/js/tonic/tonic.directive.js
@@ -5,6 +5,7 @@ app.directive('tonicDirective', function() {
 			code: '='
 		}, 
 		templateUrl: 'js/tonic/tonic.html',
+		controller: 'tonicCtrl',
 		link: function(scope, element, attributes) {
 			var notebook = Tonic.createNotebook({
 			    element: document.getElementById("my-element"),
@@ -14,4 +15,35 @@ app.directive('tonicDirective', function() {
 
 	}
 })
-<tonic-directive></tonic-directive>
+
+app.controller('tonicCtrl', function($scope) {
+	$scope.run = false;
+	$scope.tonicMsg = 'Run Tonic'
+	$scope.evalMsg = 'Run in Eval'
+	$scope.eval = false;
+	$scope.runTonic = function() {
+		if($scope.run)  {
+			$scope.run = false;
+			$scope.tonicMsg = 'Run in Tonic'
+		}
+		else {
+			$scope.run = true;
+			$scope.tonicMsg = 'Hide Tonic'
+		}
+	}
+	$scope.showEval = function() {
+		if($scope.eval)  {
+			$scope.eval = false;
+			$scope.evalMsg = 'Run in Eval'
+		}
+		else {
+			$scope.eval = true;
+			$scope.evalMsg = 'Hide Eval'
+		}
+	}
+
+	$scope.runEval = function() {
+		$scope.evalOutput = eval($scope.evalCode)
+		console.log($scope.evalOutput)
+	}
+})
