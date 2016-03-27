5e61da7e78b96394b3ebe47f0923f5f29bb9cbde
Ahertig <alyssa.hertig@gmail.com>
2016-03-26 20:05:59 -0400

    Can search from the home page

--- browser/js/notes/public-notes/public-notes.controller.js
+++ browser/js/notes/public-notes/public-notes.controller.js
@@ -1,12 +1,15 @@
-app.controller('CommunityCtrl', function($scope, NotesFactory, publicNotes) {
+app.controller('CommunityCtrl', function($scope, NotesFactory, publicNotes, $stateParams) {
 
   $scope.publicNotes = publicNotes;
+  if (!$scope.searchNotes) {
+    $scope.searchNotes = $stateParams.searchTerm;
+  }
+  console.log('the search term', $scope.searchTerm)
 
 });
 
 app.controller('NoteCtrl', function($scope, theNote) {
 
   $scope.note = theNote;
-  console.log('$scope.note', $scope.note)
 
 })
