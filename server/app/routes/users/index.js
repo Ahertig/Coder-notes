267b43c354c95cae61803c025101ea2c67466ed7
Ksenia Mikhailovskaya <ksenia0822@gmail.com>
2016-03-28 12:53:09 -0400

    got email from github and refactored createGist route

--- server/app/routes/users/index.js
+++ server/app/routes/users/index.js
@@ -3,11 +3,21 @@
 var router = require('express').Router();
 module.exports = router;
 var mongoose = require('mongoose');
+var github = require('github')
+
+router.post('/createGist', function(req, res, next) {
+	req.user.getGithubClient()
+	.then(function (client) {
+		return client.gists.createAsync(req.body)
+	})
+	.then(function(){
+		res.sendStatus(200)
+	})
+	.then(null, next)
+})
+
+
 
 router.use('/users', require('./users.js'))
 router.use('/myaccount', require('./user.js'));
 router.use('/tags', require('./user.tags.js'));
-
-
-
-
