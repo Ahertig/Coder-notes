a6a4a2b65d93b0c8719214e93bd69d1cf07a6283
zhengshizhao <zhao.zhengshi@gmail.com>
2016-04-03 16:15:09 -0400

    reset cache data when logout

--- browser/js/notes/notebook.factory.js
+++ browser/js/notes/notebook.factory.js
@@ -5,7 +5,11 @@ app.factory('NotebookFactory', function($http, $q){
     notebookCache = [];
 
 	var notebookAPI = {
-	
+	  resetNotebookCache: function(){
+      currentNotebook = undefined;
+      sharedNotebookCache = [];
+      notebookCache = [];
+    },
 		getCurrentNotebook: function() {
 			if (currentNotebook) return $q.when(currentNotebook);
 			else {
@@ -97,7 +101,7 @@ app.factory('NotebookFactory', function($http, $q){
           }
         }
       }
-    },
+    }
 
     // to be created
     // deleteNotebook: function(notebook){}
